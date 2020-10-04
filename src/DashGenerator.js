fcf75617d8b2804046f9a9e9442f9cc3762c4acd
Luca <hohmann.luca@tutanota.com>
2020-10-04 12:12:27 +0200

    Revert "Normal formats, Fallback mode"
    This reverts commit 1d1d9baad0364d9fefd0d4decc37d4c7cbfad2bd.

--- src/DashGenerator.js
+++ src/DashGenerator.js
@@ -1,30 +1,17 @@
 const ytdl = require('ytdl-core')
 const xml = require('xml-js')
-const http = require('https');
 class DashGenerator {
     // if you already have the getInfo data from ytdl-core as a string, use this function
     static generate_dash_file_from_json_data(JsonStringData, VideoLength) {
         const jsonObject = JSON.parse(JsonStringData)
-        // if (jsonObject.player_response.streamingData.hasOwnProperty("dashManifestUrl")) {
-        //     return this.get_manifest_from_url(jsonObject.player_response.streamingData.dashManifestUrl)
-        // }
-        const AdaptiveVideoFormats = jsonObject.player_response.streamingData.adaptiveFormats
-        const NormalVideoFormats = jsonObject.formats
-        let generatedJSON = this.generate_xmljs_json_from_data(NormalVideoFormats, VideoLength, false)
-        if (generatedJSON.elements[0].elements[0].elements.length === 0) {
-            // no formats
-            generatedJSON = this.generate_xmljs_json_from_data(AdaptiveVideoFormats, VideoLength, true)
-        }
+        const videoFormats = jsonObject.player_response.streamingData.adaptiveFormats
+        const generatedJSON = this.generate_xmljs_json_from_data(videoFormats, VideoLength)
         return xml.json2xml(generatedJSON)
     }
 
     // use this if you already have aformats array
-    static generate_dash_file_from_formats(NormalVideoFormats, AdaptiveVideoFormats, VideoLength) {
-        let generatedJSON = this.generate_xmljs_json_from_data(NormalVideoFormats, VideoLength, false)
-        if (generatedJSON.elements[0].elements[0].elements.length === 0) {
-            // no formats
-            generatedJSON = this.generate_xmljs_json_from_data(AdaptiveVideoFormats, VideoLength, true)
-        }
+    static generate_dash_file_from_formats(VideoFormats, VideoLength) {
+        const generatedJSON = this.generate_xmljs_json_from_data(VideoFormats, VideoLength)
         return xml.json2xml(generatedJSON)
     }
 
@@ -38,24 +25,14 @@ class DashGenerator {
         });
     }
 
-    // static get_manifest_from_url(DashUrl) {
-    //     var options = {
-    //         host: 'http://manifest.googlevideo.com',
-    //         path: DashUrl.substr(32)
-    //     };
-    //     console.log(options.path)
-    //     http.request(DashUrl, this.repon).end();
-    //     console.log(DashUrl)
-    // }
-
-    static generate_representation_audio(Format, isAdaptive) {
+    static generate_representation_audio(Format) {
         const representation =
             {
                 "type": "element",
                 "name": "Representation",
                 "attributes": {
                     "id": Format.itag,
-                    "codecs": null,
+                    "codecs": Format.mimeType.split(' ')[1].match(/"[^"]*/)[0].substr(1),
                     "bandwidth": Format.bitrate
                 },
                 "elements": [
@@ -95,22 +72,17 @@ class DashGenerator {
                     }
                 ]
             }
-            if (isAdaptive) {
-                representation.attributes.codecs = Format.mimeType.split(' ')[1].match(/"[^"]*/)[0].substr(1)
-            } else {
-                representation.attributes.codecs = Format.audioCodec
-            }
         return representation
     }
 
-    static generate_representation_video(Format, isAdaptive) {
+    static generate_representation_video(Format) {
         const representation =
             {
                 "type": "element",
                 "name": "Representation",
                 "attributes": {
                     "id": Format.itag,
-                    "codecs": null,
+                    "codecs": Format.mimeType.split(' ')[1].match(/"[^"]*/)[0].substr(1),
                     "bandwidth": Format.bitrate,
                     "width": Format.width,
                     "height": Format.height,
@@ -146,15 +118,10 @@ class DashGenerator {
                     }
                 ]
             }
-            if (isAdaptive) {
-                representation.attributes.codecs = Format.mimeType.split(' ')[1].match(/"[^"]*/)[0].substr(1)
-            } else {
-                representation.attributes.codecs = Format.videoCodec
-            }
         return representation
     }
 
-    static generate_adaptation_set(VideoFormatArray, isAdaptive) {
+    static generate_adaptation_set(VideoFormatArray) {
         const adaptationSets = []
         const mimeTypes = []
         const mimeObjects = [[]]
@@ -198,9 +165,9 @@ class DashGenerator {
             }
             mimeObjects[i].forEach((format) => {
                 if (isVideoFormat) {
-                    adapSet.elements.push(this.generate_representation_video(format, isAdaptive))
+                    adapSet.elements.push(this.generate_representation_video(format))
                 } else {
-                    adapSet.elements.push(this.generate_representation_audio(format, isAdaptive))
+                    adapSet.elements.push(this.generate_representation_audio(format))
                 }
             })
             adaptationSets.push(adapSet)
@@ -208,7 +175,7 @@ class DashGenerator {
         return adaptationSets
     }
 
-    static generate_xmljs_json_from_data(VideoFormatArray, VideoLength, isAdaptive) {
+    static generate_xmljs_json_from_data(VideoFormatArray, VideoLength) {
         const convertJSON = {
             "declaration": {
                 "attributes": {
@@ -231,7 +198,7 @@ class DashGenerator {
                         {
                             "type": "element",
                             "name": "Period",
-                            "elements": this.generate_adaptation_set(VideoFormatArray, isAdaptive)
+                            "elements": this.generate_adaptation_set(VideoFormatArray)
                         }
                     ]
                 }
